ba2ac42dafa93abee07e2e3a5b90a9a173acaec0
YuYoHan <zxzz8014@naver.com>
2024-07-14 23:10:04 +0900

    [Feat] 소셜 로그인 기능 구현

---
+++ src/controller/member/SocialLoginController.java
@@ -0,0 +1,8 @@
+package controller.member;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/app/")
+public class SocialLoginController extends HttpServlet {
+}
