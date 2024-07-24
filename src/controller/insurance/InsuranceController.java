abd0ad8c3232bf28ce6d29b7feda3f7149a2eb69
ccc700 <700eeu@gmail.com>
2024-07-24 18:29:03 +0900

    [Fix]로컬 브랜치 오류 수정(여행자 보험 페이지 연결, 헤더 메뉴 링크 수정 : 헤더 메뉴 링크 경로의 경우 추후 재수정 바랍니다)

---
+++ src/controller/insurance/InsuranceController.java
@@ -0,0 +1,40 @@
+package controller.insurance;
+
+import action.air.AirMainAction;
+import action.hotel.HotelBannerAction;
+import action.insurance.InsuranceAction;
+import config.action.ActionTo;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class InsuranceController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException, IOException {
+        String requestURI = req.getRequestURI();    // /board_mvc2/UserJoin.usrt
+        String contextPath = req.getContextPath(); // /board_mvc2
+        String command = requestURI.substring(contextPath.length());
+        ActionTo acto = null;
+
+        if (command.equals("/insurance/insurance.ns")) {
+            try {
+                acto = new InsuranceAction().execute(req, resp);
+            } catch (Exception e) {
+                System.out.println("여행자 보험 에러 : " + e.getMessage());
+            }
+        }
+
+        if (acto != null) {
+            if (acto.isRedirect()) {
+                resp.sendRedirect(acto.getPath());
+            } else {
+                RequestDispatcher disp = req.getRequestDispatcher(acto.getPath());
+                disp.forward(req, resp);
+            }
+        }
+    }
+}
---
+++ web/app/insurance/insurance.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: WD
+  Date: 2024-07-24
+  Time: 오후 6:17
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <div>
+        <h3>여행자 보험으로,<br>걱정없이 편한 여행하세요</h3>
+    </div>
+</body>
+</html>
