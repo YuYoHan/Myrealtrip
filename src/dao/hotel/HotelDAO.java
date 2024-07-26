324d0423d55b156badbe98608adbc46176cf65a8
Jaewon <dlwodnjs0128@gmail.com>
2024-07-26 12:15:31 +0900

    [Add] 숙소 페이지 구현 및 예약 테이블에 컬럼 추가, 호텔 테이블에 컬럼,데이터 추가 및 Update

--- src/dao/hotel/HotelDAO.java
+++ src/dao/hotel/HotelDAO.java
@@ -2,6 +2,8 @@ package dao.hotel;
 
 import config.jdbc.JDBCConfig;
 import dto.hotel.HotelBannerDTO;
+import dto.hotel.HotelDetailDTO;
+import dto.hotel.HotelListDTO;
 import exception.hotel.HotelException;
 
 import java.sql.Connection;
@@ -15,29 +17,90 @@ public class HotelDAO {
     public static PreparedStatement preparedStatement;
     public static ResultSet rs;
 
-    // hotel 사진을 보여주기 위해서 DB 조회해서 가져옴
-    public static List<HotelBannerDTO> hotelBannerList() {
-        String sql = "select * from hotels";
-        List<HotelBannerDTO> list = new ArrayList<HotelBannerDTO>();
+    // 전체 hotel 사진을 보여주기 위해서 DB 조회해서 가져옴
+    public static List<HotelListDTO> hotelBannerList() {
+        String sql = "SELECT h.hotel_id, h.hotel_name, hi.hotel_img_url, h.hotel_city, "
+                + "MIN(CAST(r.room_price AS DECIMAL(10, 0))) AS lowest_room_price "
+                + "FROM hotels h JOIN hotelimg hi "
+                + "ON h.hotel_id = hi.hotel_id "
+                + "JOIN rooms r "
+                + "ON h.hotel_id = r.hotel_id "
+                + "GROUP BY h.hotel_id, h.hotel_name, hi.hotel_img_url, h.hotel_city "
+                + "ORDER BY h.hotel_id";
+        List<HotelListDTO> list = new ArrayList<>();
         try {
             connection = JDBCConfig.getConnection();
             preparedStatement = connection.prepareStatement(sql);
             rs = preparedStatement.executeQuery();
 
             while (rs.next()) {
-                HotelBannerDTO hotel = new HotelBannerDTO();
-                hotel.setHotelId(rs.getInt("hotel_id"));
+                HotelListDTO hotel = new HotelListDTO();
+                hotel.setHotelID(rs.getInt("hotel_id"));
                 hotel.setHotelName(rs.getString("hotel_name"));
-                hotel.setHotelAddress(rs.getString("hotel_location"));
-                hotel.setHotelInfo(rs.getString("hotel_info"));
+                hotel.setHotelImgURL(rs.getString("hotel_img_url"));
+                hotel.setHotelCity(rs.getString("hotel_city"));
+                hotel.setRoomMinPrice(rs.getString("lowest_room_price"));
 
+                list.add(hotel);
             }
         } catch (Exception e) {
-            System.out.println("호텔 이미지 가져오는거 실패 : " + e.getMessage());
+            System.out.println("전체 호텔 리스트 가져오는거 실패 : " + e.getMessage());
             throw new HotelException(e.getMessage());
         } finally {
             JDBCConfig.close(rs, preparedStatement, connection);
         }
+        System.out.println(list);
+        return list;
+    }
+
+    // 검
+    public static List<HotelDetailDTO> hotelDetail(String hotelName) {
+        String sql = "SELECT "
+                + "h.hotel_name, "
+                + "hi.hotel_img_url, "
+                + "h.hotel_location, "
+                + "h.hotel_info, "
+                + "r.room_name, "
+                + "ri.room_img_url, "
+                + "r.room_count, "
+                + "r.room_price, "
+                + "r.option_others "
+                + "FROM hotels h JOIN hotelimg hi "
+                + "ON h.hotel_id = hi.hotel_id "
+                + "JOIN rooms r "
+                + "ON h.hotel_id = r.hotel_id "
+                + "LEFT JOIN roomimg ri "
+                + "ON r.room_id = ri.room_id "
+                + "WHERE h.hotel_name = '"+hotelName+"'"
+                + "ORDER BY h.hotel_id, r.room_id";
+
+        List<HotelDetailDTO> list = new ArrayList<>();
+        try {
+            connection = JDBCConfig.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            rs = preparedStatement.executeQuery();
+
+            while (rs.next()) {
+                HotelDetailDTO hotel = new HotelDetailDTO();
+                hotel.setHotelName(rs.getString("hotel_name"));
+                hotel.setHotelImgURL(rs.getString("hotel_img_url"));
+                hotel.setHotelLocation(rs.getString("hotel_location"));
+                hotel.setHotelInfo(rs.getString("hotel_info"));
+                hotel.setRoomImgURL(rs.getString("room_img_url"));
+                hotel.setRoomCount(rs.getInt("room_count"));
+                hotel.setRoomPrice(rs.getString("room_price"));
+                hotel.setHotelLocation(rs.getString("hotel_location"));
+                hotel.setHotelInfo(rs.getString("hotel_info"));
+
+                list.add(hotel);
+            }
+        } catch (Exception e) {
+            System.out.println("전체 호텔 리스트 가져오는거 실패 : " + e.getMessage());
+            throw new HotelException(e.getMessage());
+        } finally {
+            JDBCConfig.close(rs, preparedStatement, connection);
+        }
+        System.out.println(list);
         return list;
     }
 }
